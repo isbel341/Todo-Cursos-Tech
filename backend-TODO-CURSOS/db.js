9e9f1833ce9445418f2b55df4f2c51d5316700f5
isbel341 <isbelramon1234@gmail.com>
2025-08-21 16:31:44 -1200

    Actualización del frontend con VITE_API_URL

--- backend-TODO-CURSOS/db.js
+++ backend-TODO-CURSOS/db.js
@@ -1,12 +1,8 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // obligatorio para Neon
 });
 
 module.exports = pool;
